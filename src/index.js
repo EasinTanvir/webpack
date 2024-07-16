d8e014ffcb3c23686f0c3e03cc7d84dff4c7cb45
EasinTanvir <easintanvir102@gmail.com>
2024-07-16 23:55:10 +0600

    webpack content

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from "uuid";
+import "./styles/main.scss";
+import "./styles/custom.scss";
+import test from "./joke";
+import easinImage from "./assets/e.png";
+
+console.log(test());
+console.log(uuidv4());
+console.log("tuk");
---
+++ src/joke.js
@@ -0,0 +1,4 @@
+function test() {
+  return "This is a testing function";
+}
+export default test;
